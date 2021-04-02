5884c4eece5632114ad391c3061237aa6e54f58d
Gabriel Gaurav <gabrielgaurav@AdministorsMini.lan>
2021-04-02 11:23:54 +0530

    Payment gateway Testing

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -25,10 +25,10 @@ class StripePaymentController extends Controller
      */
     public function stripePost(Request $request)
     {
-        Stripe\Stripe::setApiKey("sk_test_51IbePbSIGJMbCciEUW9tSj4lek9vPlzaWNPzl2hvm0pW4oaRs8mxiS0NEwmj6LDohkPOdYZy8vKAqXETXQ5BG7DZ00NahKPTJg");
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
         Stripe\Charge::create ([
                 "amount" => 100 * 100,
-                "currency" => "INR",
+                "currency" => 'INR',
                 "source" => $request->stripeToken,
                 "description" => "Test payment from itsolutionstuff.com." 
         ]);
